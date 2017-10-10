949a6e312e72d8e36482d839fab1cde2d0ee9e07
jogando <jogando@jogandos-MacBook-Pro.local>
2017-10-10 09:05:25 -0300

    commit

--- medrec-network/setupDemo.sh
+++ medrec-network/setupDemo.sh
@@ -44,7 +44,6 @@ curl -X POST --header 'Content-Type: application/json' --header 'Accept: applica
  }' 'http://localhost:3000/api/Employee'
 
  #Health Sheets
-
  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
    "$class": "org.idb.medrec.HealthSheet",  
    "hsid": "hsid:1",  
@@ -95,3 +94,60 @@ curl -X POST --header 'Content-Type: application/json' --header 'Accept: applica
    ],  
    "employee": "resource:org.idb.medrec.Employee#username:jsmith"  
  }' 'http://localhost:3000/api/HealthSheet'
+
+ #UPDATE HEALTHSHEET
+ 
+ curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
+   "$class": "org.idb.medrec.UpdateHealthSheet",  
+   "healthSheet": {  
+     "$class": "org.idb.medrec.HealthSheet",  
+   "hsid": "hsid:1",  
+   "chronicConditions": [  
+     "Hypertension"  
+   ],  
+   "riskFactors": [  
+     "Coronary Artery Disease"  
+   ],  
+   "allergies": [  
+     "Penicilin",
+     "Codein"    
+   ],  
+   "other": [  
+     "string"  
+   ],  
+   "medicationRecords": [  
+     {  
+       "$class": "org.idb.medrec.MedicationRecord",  
+       "date": "05/06/2017",  
+       "medication": "Aspirin",  
+       "appearance": "White",  
+       "quantity": 2,  
+       "startDate": "05/06/2017",  
+       "endDate": "05/06/2017",  
+       "id": "string"  
+     }  
+   ],  
+   "doctorVisits": [  
+     {  
+       "$class": "org.idb.medrec.DoctorVisit",  
+       "date": "05/06/2016",  
+       "diagnosis": "Everything is ok",  
+       "medication": "none",  
+       "durationOfMedication": "none",  
+       "results": "",  
+       "id": ""  
+     },
+     {  
+       "$class": "org.idb.medrec.DoctorVisit",  
+       "date": "15/07/2017",  
+       "diagnosis": "High cholesterol levels",  
+       "medication": "Atorvastatin",  
+       "durationOfMedication": "6 months",  
+       "results": "",  
+       "id": ""  
+     }  
+   ],  
+   "employee": "resource:org.idb.medrec.Employee#username:jsmith"
+   },  
+   "timestamp": "2017-10-10T12:01:24.601Z"  
+ }' 'http://localhost:3000/api/UpdateHealthSheet'
